96e61d6b67fad399b3761dfb65b03a74502db37a
Nicolas Serna <nicolasserna@Nicolass-MacBook-Pro.local>
2020-02-27 14:31:41 -0500

    chart added

--- app.js
+++ app.js
@@ -14,23 +14,23 @@ var Chart = require('chart.js');
 let hello_string = "t";
 const app = express();
 app.set( "view engine", "ejs" ); //needed for ejs file
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+// var urlencodedParser = bodyParser.urlencoded({ extended: true })
 app.get("/",function(request,response){
-    var myChart = 0
+    var myChart = [12, 19, 3, 5, 2, 3]
     response.render("home",{myChart:myChart})
 })
-app.post("/",urlencodedParser,function(request,response){
-    var ctx = request.body.myChart
-    hello_string = 'hello'
-    console.log(ctx); 
+app.post("/",function(request,response){
+    // var ctx = request.body.myChart
+    // hello_string = 'hello'
+    // console.log(ctx); 
     response.redirect("/home")
     // return hello_string
 })
 
 
-app.listen(8000,function(){
-    console.log("server started in port 8000")
-    console.log('nope');
-    console.log(hello_string);
+app.listen(3000,function(){
+    console.log("server started in port 3000")
+    
+    // console.log(hello_string);
     
 })
